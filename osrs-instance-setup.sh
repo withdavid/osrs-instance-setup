8112809be0666b9c56355e43af70e4be775cc8e9
David Martins <DavidPentester@gmail.com>
2020-10-21 15:39:07 +0100

    Update osrs-instance-setup.sh

--- osrs-instance-setup.sh
+++ osrs-instance-setup.sh
@@ -9,8 +9,8 @@ if (whoami != root)
     then echo "You need to execute this script as root (superuser)."
     exit
 fi
-sudo echo "You need to execute this script as root (superuser)."   # Mensagem de aviso
-sudo apt-get update                                                # Atualiza os pacotes de instalacao
+
+sudo apt-get update				# Atualiza os pacotes de instalacao
 
 ###################################################################
 #             INSTALACAO E CONFIGURACAO DO AMBIENTE               #
@@ -33,7 +33,7 @@ sudo systemctl restart xrdp             # Dá restart ao servico Xrdp
 ###################################################################
 
 echo "[osrs-instance] > Installing jdk 8..."
-sudo apt-get install openjdk-8-jdk -y
+sudo apt-get install openjdk-8-jdk -y	# Instala o Jdk 8
 echo "[osrs-instance] > Java 8 has been installed!"
 java -version
 
@@ -45,8 +45,14 @@ echo "[osrs-instance] > Downloading OSBOT client..."
 sudo wget https://osbot.org/mvc/get
 mv get osbot.jar                         # Muda o nome do ficheiro
 chmod +x osbot.jar                       # Da permissoes de execucao ao ficheiro
+
 echo "[osrs-instance] > Executing OSBOT."
-sudo java -jar "OSBot.jar"               # Executa o bot
+
+cp osbot.jar ~/Desktop/osbot.jar				 										 # Copia o osbot para o desktop do xfce
+cd ~/Desktop									 										 # Entra dentro do desktop
+echo 'sudo java -jar osbot.jar' > startbot_default.sh	 								 # Cria um fichiro executavel para iniciar o bot
+echo 'sudo java -jar osbot.jar -login mendigo:osbotpwd098123#' > startbot_mendigo.sh	 # Cria um fichiro executavel para iniciar o bot
+
 
 # Dá print de algumas informações
 printf "\nCria um utilizador padrao.\n
